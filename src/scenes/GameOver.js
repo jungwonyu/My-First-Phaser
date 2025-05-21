f80d1f1441e90c2bb9f9ec06c3b2262dbd3fc959
jungwonyu <sunsetinmygarden@gmail.com>
2025-05-21 14:49:37 +0900

    first commit

---
+++ src/scenes/GameOver.js
@@ -0,0 +1,26 @@
+export class GameOver extends Phaser.Scene {
+    constructor() {
+        super('GameOver');
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0xff0000);
+
+        this.add.image(400, 300, 'background').setAlpha(0.5);
+
+        this.add.text(400, 300, 'Game Over', {
+            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        const gameScene = this.scene.get('Game');
+        const finalScore = gameScene.score;
+
+        this.add.text(400, 400, `Final Score : ${finalScore}`, {
+            fontFamily: 'Arial Black', fontSize: 30, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 4,
+            align: 'center'
+        }).setOrigin(0.5);
+    }
+}
